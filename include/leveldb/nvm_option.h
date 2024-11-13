6eea75be5cc623c92c65503abc49d5f863269d99
ErosBryant <guangxun0621@gmail.com>
2024-11-13 18:05:05 +0900

    update SSD path

--- include/leveldb/nvm_option.h
+++ include/leveldb/nvm_option.h
@@ -7,14 +7,6 @@ namespace leveldb {
 struct NVMOption {
   //SSD path
   std::string sst_path="..";
-
-  //size_t nvm_write_buffer_size = 6* 1024 * 1024 * 1024;
-  // char buf[100];
-  // snprintf(buf, sizeof(buf), "%s/_PM_sstable.pool",
-  //          NVMOption.pmem_path.c_str());
-  // std::string pol_path(buf, strlen(buf));
-
-
 };
 
 }  // namespace leveldb
